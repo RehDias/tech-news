8cea9fb3928339b7f53d1f4ae5068b50cc637693
Renata Dias <renata_dias96@live.com>
2022-11-12 20:18:06 -0300

    feat: top_5_news
    Cria uma função que lista as 5 notícias mais populares, pelo número de comentários e o desempate por ordem alfabética do título.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,12 @@
+import tech_news.database as db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_five_news = db.get_collection().find().sort([
+      ("comments_count", -1), ("title", 1)]).limit(5)
+    tuple_news = [(new['title'], new['url']) for new in top_five_news]
+    return tuple_news
 
 
 # Requisito 11
