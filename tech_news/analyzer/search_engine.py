ad4a80e563ecb942e1deae4d585e13de2d7e698d
Renata Dias <renata_dias96@live.com>
2022-11-12 18:06:26 -0300

    feat: search_by_title
    Cria função para buscar pelo título no banco de dados e retornar uma tupla com o título e a url da notícia.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+import tech_news.database as db
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = db.search_news({
+      "title": {
+        "$regex": title,
+        "$options": 'i'
+        }
+    })
+    tuple_news = [(new['title'], new['url']) for new in news]
+    return tuple_news
 
 
 # Requisito 7
